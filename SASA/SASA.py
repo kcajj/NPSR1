63a7934a89484e912c01e3d11fa514efec26cf8e
AnnaSalvadori <124202886+AnnaSalvadori@users.noreply.github.com>
2024-01-06 11:15:57 +0100

    Remove unused functions

--- SASA/SASA.py
+++ SASA/SASA.py
@@ -44,36 +44,6 @@ def parse_dssp(file, chain):
     return dssp_list, SA_count
 
 
-def retrieve_interface(filename): 
-    ''' Reads a rtf or txt file containing the H-bonds involved in the interaction between the ligand (NPS, chain B) and the protein (NPSR, chain A)
-        Parameter: name of the rtf or txt file '''
-    
-    # Initialise the list
-    interface_list = []
-
-    # Read the rtf or txt file and read each line
-    file = open(filename)
-    for line in file:
-        line = line.rstrip()
-        if line == "": # skip empty lines
-            continue
-
-        # Read lines containing the H-bonds records 
-        if line[0] == "/":
-            donor = (line[1], int(line[7:10].strip())) # donor chain and coordinate
-            acceptor = (line[17], int(line[23:26].strip())) # acceptor chain and coordinate
-            
-            # Flag to easily retrieve the coordinate in the function "get_interface_acc"
-            flag = 0
-            if "A" in acceptor:
-                flag = 1
-            
-            # Add the variables to the list
-            interface_list.append([donor, acceptor, flag])
-
-    return interface_list
-
-
 def get_total_SASA(dssp_list):
     ''' Returns the total solvent-accessible surface area (SASA) of the protein/peptide.
         Parameter: the list containing amino acid, accessiblity and residue number (derived from the function "parse_dssp) '''
@@ -108,38 +78,6 @@ def get_transmembrane_SASA(dssp_list, tm_list):
 
     return sum(accs) # return the sum of accessibilities
 
- 
-def get_interface_SASA(dssp_list, interface_list):
-    ''' Returns the total solvent-accessible surface area (SASA) of the interface of the protein, given its binding with the ligand.
-        Parameter: the list containing amino acid, accessiblity and residue number (derived from the function "parse_dssp), and the list of the interface residues (derived from ChimeraX using the docking model) '''
-
-    # Initialisation of the accessibilities list
-    accs = []
-
-    # Create a new interaction list, containing the unique coordinates of the residues involved in binding
-    aa_inter_list = []
-    for interaction in interface_list:
-        aa_number = interaction[interaction[2]][1] # choose chain A (receptor) interface residue coordinates according to the flag added in the function "retrieve_interface"
-        aa_inter_list.append(aa_number)
-    original_list = aa_inter_list.copy() # copy the original list
-    print("Total number of H bonds:", len(aa_inter_list))
-    aa_inter_list = list(set(aa_inter_list)) # remove duplicates
-    aa_inter_list.sort() # sort list 
-
-    if verbose == "-v":
-        print("Coordinates of residues involved in the interaction:", aa_inter_list)
-        print("Number of residues involved in the interaction:", len(aa_inter_list))
-    
-    # Add the accessibility only of residues belonging to the interface list, to the accessibilities list
-    for coordinate in aa_inter_list:
-        aa = dssp_list[coordinate-1]
-        accs.append(aa[1])
-        
-        if verbose == "-v":
-            print(f"\tThe residue {aa[0]} with coordinate {coordinate} is involved in interaction through {original_list.count(coordinate)} H-bond(s)")
-    
-    return sum(accs) # return the sum of accessibilities
-
 
 def interaction_surface(s_nps, s_npsr, s_npsr_inter, s_nps_inter):
     ''' Computes the surface of interaction between NPS and NPSR1
@@ -172,10 +110,6 @@ if __name__ == '__main__':
     file_NPS = sys.argv[1]
     file_NPSR = sys.argv[2]
     file_docking = sys.argv[3]
-    if len(sys. argv) == 5:
-        verbose = sys.argv[4]
-    else:
-        verbose = ""
 
     # Create the inputs
     inputs = [
@@ -221,14 +155,6 @@ if __name__ == '__main__':
                 # the coordinates were obtained with ChimeraX
             tm_SASA = get_transmembrane_SASA(dssp_list, tm_coordinates)
             print(f"Transmembrane SASA for NPSR: {tm_SASA} Å^2")
-
-        # 3. Interface and interaction analysis
-        if file == "NPSR.dssp":
-            print("\nINTERFACE ANALYSIS")
-            interface_list = retrieve_interface("interface.rtf")
-                # the file "interface.rtf" was obtained with ChimeraX using the docking model
-            inter_SASA = get_interface_SASA(dssp_list, interface_list)
-            print("Interface SASA for NPSR: ", inter_SASA)
     
     print("")
 
