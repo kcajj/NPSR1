813a91968ba6b3add602034456209d9013d7ebcf
Giacomo <thejack2307@gmail.com>
2024-01-07 13:36:18 +0100

    note that explains scripts finished

--- scripts/fix_docking_output.py
+++ scripts/fix_docking_output.py
@@ -1,28 +1,45 @@
-if __name__=="__main__":
-    input_filename="model.000.09.pdb"
-    output_filename="docking_model.pdb"
+def fix_pdb(input_filename, output_filename):
+    '''
+    takes in input the old file and the new one. It finds where the first chain stops,
+    then over writes all the successive lines fixing the chain label and the ID.
+    '''
 
     b=0
     pdb=open(input_filename,"r")
     new_pdb=open(output_filename,"w")
+
     for line in pdb:
+
         #if it is the second chain
         if line=="HEADER lig.000.09.pdb\n":
-            b=1
+            b=1 #stop skipping the lines
             new_pdb.write(line)
             continue
+        
+        #skip some extra lines that are present in the file
+        if line[:3]=="REM" or line[:3]=="TER":
+            continue
+
+        #if it is the first chain
         if b==0:
-            # for the first chain just copy each line
+            #just copy each line
             new_pdb.write(line)
 
-            #take the id:
+            #take the id of the atom
             piece=line[7:11].strip()
             if piece != '':
                 if piece[0] in "1234567890":
                     last_id=int(piece)
-
             continue
+
+        #if it is the second chain
         else:
             last_id+=1
-            new_line=line[:7]+str(last_id)+line[11:21]+'B'+line[22:]
+            new_line=line[:7]+str(last_id)+line[11:21]+'B'+line[22:] #rebuild the line
             new_pdb.write(new_line)
+
+if __name__=="__main__":
+    input_filename="structure_models/docking_output.pdb"
+    output_filename="structure_models/docking_model.pdb"
+
+    fix_pdb(input_filename, output_filename)
--- scripts/note.md
+++ scripts/note.md
@@ -1,5 +1,13 @@
 # Explanation of the scripts
 
+## Fixing the docking output
+
+The docking model produced by ClusPro had some problems. We manipulated the file through a Python script to correct it and to allow its visualisation and subsequent analyses.
+
+In the original file, both chains were labelled as "A" and the IDs of the initial atoms in the two chains were identical. The script allows to rename the second chain and to make its IDs unique. The original file also presented some extra lines that caused troubles during visualisation, these lines were removed.
+
+By running "fix_docking_output.py" we obtain a perfect pdb file named "docking_model.pdb".
+
 ## SASA
 The Python script "SASA.py" is a tool to retrieve the solvent-accessible surface area (SASA) of a protein (NPSR1) as a whole, of its transmembrane domain and of its interface (considering the specific ligand NPS). It can be also used on a peptide (NPS).
 
@@ -9,7 +17,6 @@ python3 SASA.py NPS.dssp NPSR.dssp docking_model.dssp
 ```
 
 Many results will be printed. Among them, there are the SASA values for NPS and NPSR both in their monomeric forms and in the complex, the surface of interaction and the residues that are predicted to be involved in the interaction.
-
 
 ## Amino acids composition
 The python scripts "functionsFASTA.py" and "functionsGFF.py" contain functions to analyse the amino acid relative abundance in different conditions: in the whole protein and transmembrane domain, both by counting each amino acid or by property. The output of the functions is used to create barplots of the relative abundances.
